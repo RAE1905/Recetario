539ddab27b78e8aaaed934fff16b63ec4e9fb0f0
RAE1905 <raes1905raes@gmail.com>
2023-05-04 11:38:22 -0600

    RecetarioV1

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,12 @@ module.exports = {
       filename: 'index.html',
       template: './src/index.html'
     })
-  ]
+  ],
+devServer : {
+  static: {
+    directory: path.join(__dirname, 'public'),
+  },
+  port: 3000,
+  hot: true,
+}
 }
